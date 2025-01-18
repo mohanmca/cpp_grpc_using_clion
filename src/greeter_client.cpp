72b668471c8909d00bcbba29d34b72cefafd160f
Mohan Narayanaswamy <mohanmca@gmail.com>
2025-01-18 16:44:40 +0800

    build-grpc

---
+++ src/greeter_client.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <grpcpp/grpcpp.h>
+#include "hello_world.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+
+class GreeterClient {
+public:
+    GreeterClient(std::shared_ptr<Channel> channel) : stub_(Greeter::NewStub(channel)) {}
+
+    std::string SayHello(const std::string& user) {
+        HelloRequest request;
+        request.set_name(user);
+
+        HelloReply reply;
+        ClientContext context;
+
+        Status status = stub_->SayHello(&context, request, &reply);
+
+        if (status.ok()) {
+            return reply.message();
+        } else {
+            std::cout << "RPC failed" << std::endl;
+            return "RPC failed";
+        }
+    }
+
+private:
+    std::unique_ptr<Greeter::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+    GreeterClient greeter(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+    std::string user("world");
+    std::string reply = greeter.SayHello(user);
+    std::cout << "Greeter received: " << reply << std::endl;
+    return 0;
+}
